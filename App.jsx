59e55e5234eb3f635359802471cf58a2cbe22a47
Irfan Oktora <irfanoktora.dev@gmail.com>
2023-08-07 15:05:33 +0700

    add react-icon

--- App.jsx
+++ App.jsx
@@ -1,7 +1,12 @@
+import { CgHello } from "react-icons/cg";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        <CgHello />
+        Hello world!
+      </h1>
     </>
   );
 }
