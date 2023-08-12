d3130f724aa7a02d13a6105f8bf861296419d47d
Irfan Oktora <irfanoktora.dev@gmail.com>
2023-08-12 15:19:30 +0700

    add new app.jsx

--- App.jsx
+++ App.jsx
@@ -7,6 +7,10 @@ export default function App() {
         <CgHello />
         Hello world!
       </h1>
+      <div className="join">
+        <input className="input input-bordered join-item" placeholder="Email" />
+        <button className="btn join-item rounded-r-full">Subscribe</button>
+      </div>
     </>
   );
 }
